246a6810d7ebe53051d035883b5893f880af7dd5
Sathvik Dantu <sathvikdantu45@gmail.com>
2026-03-05 14:11:56 +0530

    Fix notification module exports, payment module dependency injection, and database package config

--- apps/api/src/notification/notification.module.ts
+++ apps/api/src/notification/notification.module.ts
@@ -1,13 +1,26 @@
 import { Module } from "@nestjs/common";
 import { BullModule } from "@nestjs/bullmq";
 import { HttpModule } from "@nestjs/axios";
+
 import { NotificationProcessor } from "./notification.processor";
 import { WhatsAppService } from "./whatsapp.service";
 import { NotificationLogService } from "./notification-log.service";
+import { NotificationService } from "./notification.service";
 
 @Module({
-  imports: [BullModule.registerQueue({ name: "notifications" }), HttpModule],
-  providers: [NotificationProcessor, WhatsAppService, NotificationLogService],
-  exports: [WhatsAppService],
+  imports: [
+    BullModule.registerQueue({ name: "notification" }),
+    HttpModule,
+  ],
+  providers: [
+    NotificationService,
+    NotificationProcessor,
+    WhatsAppService,
+    NotificationLogService,
+  ],
+  exports: [
+    NotificationService,   // ⭐ REQUIRED
+    WhatsAppService,
+  ],
 })
 export class NotificationModule {}
